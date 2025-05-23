34e46be76e7219875268681287f20664a7c52a78
vidya <vidyar1926@gmail.com>
2025-05-23 10:22:22 +0530

    Photon Script

--- test1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://leaftaps.com/opentaps/control/main');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).click();
-});
